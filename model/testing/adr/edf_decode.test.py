15c139433aa82c10dec774ef97592a6768279e37
Andrew De Rango <drewderango@gmail.com>
2025-01-30 22:09:39 -0500

    preliminary decoding

---
+++ model/testing/adr/edf_decode.test.py
@@ -0,0 +1,14 @@
+import os
+import mne
+
+# Load the EDF file
+edf_file = os.path.join('data', 'physionet', 'sleep-cassette', 'SC4001E0-PSG.edf')
+raw = mne.io.read_raw_edf(edf_file, preload=True)
+
+# print basic info
+print(raw.info)
+
+# convert whole thing to dataframe
+df = raw.to_data_frame()
+print(df.head())
+print(df.tail())
