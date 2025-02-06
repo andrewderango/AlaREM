0b07af74e3d6a750b0df5a1611b726c794f4d398
Andrew De Rango <drewderango@gmail.com>
2025-02-06 14:16:24 -0500

    get all datafranes

--- model/preprocessing_functions.py
+++ model/preprocessing_functions.py
@@ -13,13 +13,13 @@ for edf_file in edf_files:
         continue
     print(edf_file)
 
-quit()
-
-
-edf_file = os.path.join('data', 'physionet', 'sleep-cassette', 'SC4001E0-PSG.edf')
-raw = mne.io.read_raw_edf(edf_file, preload=True)
-
-# convert whole thing to dataframe
-df = raw.to_data_frame()
-df = df[['time', 'EEG Fpz-Cz', 'EEG Pz-Oz']]
-print(df.head())
+    if edf_file[1] == 'T':
+        raw = mne.io.read_raw_edf(os.path.join('data', 'physionet', 'sleep-telemetry', edf_file), preload=True)
+    elif edf_file[1] == 'C':
+        raw = mne.io.read_raw_edf(os.path.join('data', 'physionet', 'sleep-cassette', edf_file), preload=True)
+    else:
+        raise ValueError('Invalid file name')
+    
+    df = raw.to_data_frame()
+    df = df[['time', 'EEG Fpz-Cz', 'EEG Pz-Oz']]
+    print(df.head())
