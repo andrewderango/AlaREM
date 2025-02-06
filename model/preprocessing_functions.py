ac190cea75df4a7e66d63e45c4f80e5372ffe9de
Andrew De Rango <drewderango@gmail.com>
2025-02-06 15:45:54 -0500

    add data type, subject number, night number

--- model/preprocessing_functions.py
+++ model/preprocessing_functions.py
@@ -12,15 +12,22 @@ for edf_file in edf_files:
     print(edf_file)
 
     if edf_file[1] == 'T':
-        raw = mne.io.read_raw_edf(os.path.join('data', 'physionet', 'sleep-telemetry', edf_file), preload=True)
+        raw = mne.io.read_raw_edf(os.path.join('data', 'physionet', 'sleep-telemetry', edf_file), preload=True, verbose=False)
+        data_type = 'telemetry'
     elif edf_file[1] == 'C':
-        raw = mne.io.read_raw_edf(os.path.join('data', 'physionet', 'sleep-cassette', edf_file), preload=True)
+        raw = mne.io.read_raw_edf(os.path.join('data', 'physionet', 'sleep-cassette', edf_file), preload=True, verbose=False)
+        data_type = 'cassette'
     else:
         raise ValueError('Invalid file name')
+    subject_number = edf_file[3:5]
+    night_number = edf_file[5]
     
     df = raw.to_data_frame()
     df = df[['time', 'EEG Fpz-Cz', 'EEG Pz-Oz']]
-    # add col for personID
-    # add col for nightID
+    df['type'] = data_type
+    df['subject'] = subject_number
+    df['night'] = night_number
+
     # add col for EpochID
+
     print(df.head())
